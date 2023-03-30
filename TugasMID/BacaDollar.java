c990c5d9c75a6ddb344f66140639879fbe260661
Rahmapuspitasari30 <127001026+Rahmapuspitasari30@users.noreply.github.com>
2023-03-30 11:58:08 +0700

    Add files via upload

---
+++ TugasMID/BacaDollar.java
@@ -0,0 +1,20 @@
+//Rahma Puspitasari 13020210093
+
+import java.util.Scanner;
+
+public class BacaDollar{
+	public static void main(String[] args)
+	{
+		int Dollar;
+		int Rupiah;
+
+		System.out.println("Masukkan Dollar :");
+		Scanner input = new Scanner(System.in);
+		int i = input.nextInt();
+		Dollar = 1509480;
+		Rupiah = Dollar;
+
+	System.out.println("Hasil"+i +" Dollar="+Rupiah+"Rupiah");
+	}
+
+}
